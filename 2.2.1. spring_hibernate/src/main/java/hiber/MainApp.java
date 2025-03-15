354f39733e0f0311ba036d26c5f17debc19f1e96
MihasBatler <katrin.bat-darling@yandex.ru>
2025-03-15 13:21:28 +0500

    Initial commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -10,24 +11,44 @@ import java.util.List;
 
 public class MainApp {
    public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+      AnnotationConfigApplicationContext context =
+              new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+
+      User user1 = new User("Чичи", "Рис", "user1@mail.ru");
+      User user2 = new User("Чючю", "Петрова", "user2@mail.ru");
+      User user3 = new User("Чюча", "Янг", "user3@mail.ru");
+      User user4 = new User("Чичю", "Чан", "user4@mail.ru");
+      User user5 = new User("Тарас", "Бульба", "user5@mail.ru");
+
+      Car mercedes = new Car("mercedes", 17);
+      Car hyundai = new Car("hyundai", 424);
+      Car nissan = new Car("nissan", 110);
+      Car toyota = new Car("toyota", 100);
+      Car mitsubishi = new Car("mitsubishi", 490);
+
+
+      userService.add(user1.setCar(mercedes).setUser(user1));
+      userService.add(user2.setCar(hyundai).setUser(user2));
+      userService.add(user3.setCar(nissan).setUser(user3));
+      userService.add(user4.setCar(toyota).setUser(user4));
+      userService.add(user5.setCar(mitsubishi).setUser(user5));
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
+         System.out.println("Id = " + user.getId());
+         System.out.println("First Name = " + user.getFirstName());
+         System.out.println("Last Name = " + user.getLastName());
+         System.out.println("Email = " + user.getEmail());
+         System.out.println("Car = " + user.getCar());
          System.out.println();
       }
+
+      System.out.println(userService.getUserByCar("mercedes", 17));
+
 
       context.close();
    }
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -1,5 +1,6 @@
 package hiber.model;
 
+
 import javax.persistence.*;
 
 @Entity
@@ -8,6 +9,7 @@ public class User {
 
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name = "id")
    private Long id;
 
    @Column(name = "name")
@@ -19,8 +21,13 @@ public class User {
    @Column(name = "email")
    private String email;
 
-   public User() {}
-   
+
+   @OneToOne(cascade = CascadeType.ALL, mappedBy = "user")
+   private Car car;
+
+   public User() {
+   }
+
    public User(String firstName, String lastName, String email) {
       this.firstName = firstName;
       this.lastName = lastName;
@@ -57,5 +64,25 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+
+   public Car getCar() {
+      return car;
+   }
+
+   public Car setCar(Car car) {
+      this.car = car;
+      return car;
+   }
+
+   @Override
+   public String toString() {
+      return "User{" +
+              "id=" + id +
+              ", firstName='" + firstName + '\'' +
+              ", lastName='" + lastName + '\'' +
+              ", email='" + email + '\'' +
+              ", car=" + car +
+              '}';
    }
 }
